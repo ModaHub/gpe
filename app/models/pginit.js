7bc5022344b950d78b9878e81b6b0ed640915db4
Bourenane Nabil <bouren.n@gmail.com>
2016-02-17 18:52:37 +0100

    Fix DB

--- app/models/pginit.js
+++ app/models/pginit.js
@@ -1,11 +1,11 @@
 var pg = require('pg');
-var db = require('../config/db');
+var db = require('../../config/db');
 
-var client = new pg.Client(db.connect);
+var client = new pg.Client(db.url);
 
 client.connect();
 
-var query = client.query('CREATE TABLE (id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 	query.on('end', function() {
 		client.end();
 	});
