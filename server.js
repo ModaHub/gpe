68372dde8ddca71399ac08c5eb3761fa87ee87b7
Thierry THONGKHAM <thongk_t@etna-alternance.net>
2016-02-20 03:10:48 +0100

    Routing degueulasse, a refaire

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 
 // modules =================================================
-var express			= require('express');
-var app				= express();
-var bodyParser			= require('body-parser');
-var methodOverride		= require('method-override');
+var express	        = require('express');
+var app	            = express();
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var requireDir      = require('require-dir');
+var routes          = requireDir('./app/routes', {recurse: true});
 
 // configuration ===========================================
 var port = process.env.npm_package_config_port || 8080;
@@ -14,20 +16,28 @@ exports = module.exports = app;
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // routes ==================================================
-require('./app/routes')(app);
-
+for (var dir in routes) {
+    for (var subdir in routes[dir]) {
+        for (var file in routes[dir][subdir]) {
+            if (Object.keys(routes[dir][subdir][file]).length > 0) {
+                console.log(routes[dir][subdir][file]);
+                app.use('/', routes[dir][subdir][file]);
+            }
+        }
+    }
+}
 // start app ===============================================
 app.listen(port);
 
