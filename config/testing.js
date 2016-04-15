cc9ae93201f18e50a4e2dbad79c460a054f50628
Thierry THONGKHAM <thongk_t@etna-alternance.net>
2016-04-15 14:16:00 +0200

    Ajout de fichiers d'environnement

---
+++ config/testing.js
@@ -0,0 +1,9 @@
+// config/test.js
+var os = require('os');
+
+module.exports = {
+    hostname: os.hostname(),
+    ip: 'localhost',
+    port: "8080",
+    db_url: "postgresql://root:etna42@37.59.60.163:5432/ttk"
+}
