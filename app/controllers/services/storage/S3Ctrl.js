4ade51b2831365e0dbf54d25dd138431d12155da
Teegie <aoubid_h@etna-alternance.net>
2016-09-04 21:37:50 +0200

    modification de la route de synchronisation

--- app/controllers/services/storage/S3Ctrl.js
+++ app/controllers/services/storage/S3Ctrl.js
@@ -1,68 +1,60 @@
 // app/controllers/services/storage/S3Ctrl.js
-var storage = require('./storageCtrl.js');
+var storage     = require('./storageCtrl.js');
+var bucketUtils = require('../../../utils/bucketUtils.js');
 
 // ======================= GET =======================
 module.exports.syncAWSContainers = function(req, res) {
-  var QRB = req.app.get('QRB');
-  var awsTools = req.app.get('awsTools');
+    var QRB       = req.app.get('QRB');
+    var awsTools  = req.app.get('awsTools');
 
-  QRB('accounts')
-  .where({
-    user_id: '1',
-    cloud_vendor: 'aws'
-  })
-  .select()
-  .first()
-  .then(function (datas) {
-    if (null === datas)
-    return res.status(200).json([]);
-    else {
-      awsTools.config.update({
-        accessKeyId:     datas.aws_access_key_id,
-        secretAccessKey: datas.aws_secret_access_key_id
-      });
-      var s3 = new awsTools.S3({signatureVersion: 'v4'});
-
-      s3.listBuckets(function(err, data) {
-        if (err) console.log(err, err.codeStatus, err.message);
-        else {
-          for (var i in data.Buckets) {
-            console.log(data.Buckets[i]);
-
-            var bucket = {
-              name:            data.Buckets[i].Name,
-              description:     '',
-              creation_date:   '2016-08-25 19:55:38.742333+02',
-              acl:             'private',
-              storage_class:   'STANDARD_IA',
-              region:          '',
-              size:            '0',
-              expect:          '1',
-              request_payment: '0',
-              versionning:     '0',
-              storage_id:      req.storage.id
-            };
-
-            QRB.returning('*')
-            .insert(bucket)
-            .into('aws_storage_containers')
-            .then(function (container) {
-              return res.status(201).json(container);
-            })
-            .catch(function (error) {
-              return res.status(400).json({
-                msg:   "Error when writing datas",
-                error: error
-              });
-            });
-          }
+    QRB('accounts')
+    .where({
+        user_id: '1',
+        cloud_vendor: 'aws'
+    })
+    .select()
+    .first()
+    .then(function (datas) {
+        if (null === datas) {
+          return res.status(200).json([]);
         }
-      });
-    }
-  })
-  .catch(function(error) {
+        else {
+            awsTools.config.update({
+                accessKeyId:     datas.aws_access_key_id,
+                secretAccessKey: datas.aws_secret_access_key_id
+            });
+            var s3      = new awsTools.S3({signatureVersion: 'v4'});
+            var buckets = new Array();
+
+            s3.listBuckets(function(err, data) {
+                if (err) {
+                    return res.status(500).json({
+                        s3:   'listBuckets',
+                        code: err.codeStatus,
+                        msg:  err.message,
+                    });
+                }
+                buckets = bucketUtils.formatBuckets(s3, data.Buckets, req, res);
+
+                return res.status(200).json(buckets);
+                QRB.returning('*')
+                .insert(buckets)
+                .into('aws_storage_containers')
+                .then(function (container) {
+                    return res.status(201).json(container);
+                })
+                .catch(function (error) {
+                    return res.status(400).json({
+                        msg:   "Error when writing datas",
+                        error: error
+                    });
+                });
+            });
+        }
+    })
+    .catch(function(error) {
     res.status(400).json(error);
-  });
+    });
 }
 
 // ======================= POST =======================
